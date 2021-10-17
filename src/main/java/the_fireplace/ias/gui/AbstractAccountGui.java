fb7152ca89728a5ca609ae2b143a67ba1ebbe04e
VidTuGit <60929459+VidTuGit@users.noreply.github.com>
2021-10-17 12:28:41 +0300

    Update to 7.1.3.

--- src/main/java/the_fireplace/ias/gui/AbstractAccountGui.java
+++ src/main/java/the_fireplace/ias/gui/AbstractAccountGui.java
@@ -1,70 +1,89 @@
 package the_fireplace.ias.gui;
 
-import org.lwjgl.glfw.GLFW;
+import java.util.List;
+import java.util.function.Consumer;
 
-import com.github.mrebhan.ingameaccountswitcher.tools.alt.AccountData;
-import com.github.mrebhan.ingameaccountswitcher.tools.alt.AltDatabase;
+import org.lwjgl.glfw.GLFW;
 
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.resource.language.I18n;
 import net.minecraft.text.Text;
-import the_fireplace.iasencrypt.EncryptionTools;
+import net.minecraft.text.TranslatableText;
+import ru.vidtu.ias.Config;
+import ru.vidtu.ias.account.Account;
+import ru.vidtu.ias.account.AuthException;
+import ru.vidtu.ias.account.MojangAccount;
+import ru.vidtu.ias.account.OfflineAccount;
+import ru.vidtu.ias.gui.MSAuthScreen;
+import ru.vidtu.ias.utils.Auth;
+import ru.vidtu.ias.utils.SkinRenderer;
+import the_fireplace.ias.IAS;
 
 /**
+ * Screen for adding Mojang and Offline accounts.
  * @author evilmidget38
  * @author The_Fireplace
  */
-public abstract class AbstractAccountGui extends Screen {
+public class AbstractAccountGui extends Screen {
 	public final Screen prev;
 	private TextFieldWidget username;
-	private GuiPasswordField password;
+	private TextFieldWidget password;
 	private ButtonWidget complete;
-	protected boolean hasUserChanged = false;
-
-	public AbstractAccountGui(Screen prev, Text actionString) {
-		super(actionString);
+	private boolean logging;
+	private Consumer<Account> handler;
+	private List<String> error;
+	
+	public AbstractAccountGui(Screen prev, Text title, Consumer<Account> handler) {
+		super(title);
 		this.prev = prev;
+		this.handler = handler;
 	}
 	
 	@Override
-	protected void init() {
-		addButton(complete = new ButtonWidget(this.width / 2 - 152, this.height - 28, 150, 20, this.title.asFormattedString(), btn -> {
-			complete();
-			escape();
-		}));
-		addButton(new ButtonWidget(this.width / 2 + 2, this.height - 28, 150, 20, I18n.translate("gui.cancel"), btn -> escape()));
-		addButton(username = new TextFieldWidget(this.font, this.width / 2 - 100, 60, 200, 20, ""));
+	public void init() {
+		complete = addButton(new ButtonWidget(this.width / 2 - 152, this.height - 28, 150, 20, this.title.asFormattedString(), btn -> end()));
+		addButton(new ButtonWidget(this.width / 2 + 2, this.height - 28, 150, 20, I18n.translate("gui.cancel"), btn -> minecraft.openScreen(prev)));
+		username = addButton(new TextFieldWidget(font, this.width / 2 - 100, 60, 200, 20, I18n.translate("ias.username")));
 		username.setMaxLength(512);
-		addButton(password = new GuiPasswordField(this.font, this.width / 2 - 100, 90, 200, 20, ""));
+		password = addButton(new GuiPasswordField(font, this.width / 2 - 100, 90, 200, 20, I18n.translate("ias.password")));
 		password.setMaxLength(512);
 		complete.active = false;
+		addButton(new ButtonWidget(this.width / 2 - 50, this.height / 3 * 2, 100, 20, I18n.translate("ias.msauth.btn"), btn -> minecraft.openScreen(new MSAuthScreen(prev, handler))));
 	}
 	
 	@Override
 	public void render(int mx, int my, float delta) {
 		renderBackground();
-		this.drawCenteredString(font, this.title.asFormattedString(), this.width / 2, 7, -1);
-		this.drawCenteredString(font, I18n.translate("ias.username"), this.width / 2 - 130, 66, -1);
-		this.drawCenteredString(font, I18n.translate("ias.password"), this.width / 2 - 130, 96, -1);
+		drawCenteredString(font, this.title.asFormattedString(), this.width / 2, 7, -1);
+		drawCenteredString(font, I18n.translate("ias.username"), this.width / 2 - 130, 66, -1);
+		drawCenteredString(font, I18n.translate("ias.password"), this.width / 2 - 130, 96, -1);
+		if (error != null) {
+			for (int i = 0; i < error.size(); i++) {
+				drawCenteredString(font, error.get(i), this.width / 2, 114 + i * 10, 0xFFFF0000);
+				if (i > 6) break; //Exceptions can be very large.
+			}
+		}
 		super.render(mx, my, delta);
 	}
 	
 	@Override
 	public boolean keyPressed(int key, int oldkey, int mods) {
 		if (key == GLFW.GLFW_KEY_ESCAPE) {
-			escape();
-		} else if (key == GLFW.GLFW_KEY_ENTER) {
+			minecraft.openScreen(prev);
+			return true;
+		}
+		if (key == GLFW.GLFW_KEY_ENTER) {
 			if (username.isFocused()) {
 				username.setSelected(false);
 				password.setSelected(true);
-			} else if (password.isFocused() && complete.active) {
-				complete();
-				escape();
+				return true;
 			}
-		} else {
-			if (username.isFocused()) hasUserChanged = true;
+			if (password.isFocused() && complete.active) {
+				end();
+				return true;
+			}
 		}
 		return super.keyPressed(key, oldkey, mods);
 	}
@@ -76,47 +95,57 @@ public abstract class AbstractAccountGui extends Screen {
 	
 	@Override
 	public void tick() {
-		complete.active = canComplete();
+		complete.active = !username.getText().trim().isEmpty() && !logging;
+		complete.setMessage(!username.getText().trim().isEmpty() && password.getText().isEmpty()?this.title.copy().append(" ").append(new TranslatableText("ias.offline")).asFormattedString():this.title.asFormattedString());
+		username.active = password.active = !logging;
 		username.tick();
 		password.tick();
 		super.tick();
 	}
 
-	private void escape(){
-		minecraft.openScreen(prev);
+	public void end() {
+		if (password.getText().isEmpty()) {
+			String name = username.getText();
+			if (Config.accounts.stream().anyMatch(acc -> acc.alias().equalsIgnoreCase(name))) {
+				error = font.wrapStringToWidthAsList(I18n.translate("ias.auth.alreadyexists"), width - 10);
+				return;
+			}
+			logging = true;
+			new Thread(() -> {
+				SkinRenderer.loadSkin(minecraft, name, null, false);
+				minecraft.execute(() -> {
+					if (minecraft.currentScreen == this) {
+						handler.accept(new OfflineAccount(username.getText()));
+						minecraft.openScreen(prev);
+					}
+				});
+				logging = false;
+			}).start();
+		} else {
+			String name = username.getText();
+			String pwd = password.getText(); 
+			logging = true;
+			new Thread(() -> {
+				try {
+					MojangAccount ma = Auth.authMojang(name, pwd);
+					SkinRenderer.loadSkin(minecraft, ma.alias(), ma.uuid(), false);
+					if (Config.accounts.stream().anyMatch(acc -> acc.alias().equalsIgnoreCase(name)))
+						throw new AuthException(new TranslatableText("ias.auth.alreadyexists"));
+					minecraft.execute(() -> {
+						if (minecraft.currentScreen == this) {
+							handler.accept(ma);
+							minecraft.openScreen(prev);
+						}
+					});
+				} catch (AuthException ae) {
+					IAS.LOG.warn("Unable to add account (expected exception)", ae);
+					minecraft.execute(() -> error = font.wrapStringToWidthAsList(ae.getText().asFormattedString(), width - 10));
+				} catch (Throwable t) {
+					IAS.LOG.warn("Unable to add account (unexpected exception)", t);
+					minecraft.execute(() -> error = font.wrapStringToWidthAsList(I18n.translate("ias.auth.unknown", t.getLocalizedMessage()), width - 10));
+				}
+				logging = false;
+			}, "IAS Mojang Auth Thread").start();
+		}
 	}
-
-	public String getUsername()
-	{
-		return username.getText();
-	}
-
-	public String getPassword()
-	{
-		return password.getText();
-	}
-
-	public void setUsername(String username)
-	{
-		this.username.setText(username);
-	}
-
-	public void setPassword(String password)
-	{
-		this.password.setText(password);
-	}
-
-	protected boolean accountNotInList(){
-		for(AccountData data : AltDatabase.getInstance().getAlts())
-			if(EncryptionTools.decode(data.user).equals(getUsername()))
-				return false;
-		return true;
-	}
-
-	public boolean canComplete()
-	{
-		return getUsername().length() > 0 && accountNotInList();
-	}
-
-	public abstract void complete();
 }
